431ec78fcd1c86fe46e1473bbe820dc4735aebeb
poojauttam122 <98681730+poojauttam122@users.noreply.github.com>
2022-01-31 11:40:52 +0530

    by using for loop

--- addition.java
+++ addition.java
@@ -1,7 +1,10 @@
 class addition{
-int n1,n2,n3;
+int n1,n2,n3,count=10,i;
 public static void main(args[]){
-n3=n1+n2;
+for(i=1;i<=count;i++)
+{
+ n3=n1+n2;
 systeam.out.println(add two number +n3);
+}
 }
 }
